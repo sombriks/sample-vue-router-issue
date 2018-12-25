b4f54dcc11005a8020d72d126383e016f0c8715b
sombriks <sombriks@gmail.com>
2018-12-25 15:38:52 -0300

    sampling issue as seen on https://stackoverflow.com/questions/53917029/i-can-redirect-to-a-different-route-but-that-route-doesnt-work-if-i-refresh-th

---
+++ src/views/Day.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <pre>{{$route.params.year}}-{{$route.params.month}}-{{$route.params.day}}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"day",
+}
+</script>
+
+<style>
+
+</style>
